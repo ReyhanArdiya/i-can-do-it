5cf7d882b8c22942cd2769362baa268bc4957169
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-10-03 08:35:34 +0700

    Setup `chakra-ui`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
+import { Center } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 
 const Home: NextPage = () => {
-    return <div>Meow</div>;
+    return (
+        <Center fontSize="20rem" color="blue.900" h="100vh" w="full">
+            Meowmere
+        </Center>
+    );
 };
 
 export default Home;
