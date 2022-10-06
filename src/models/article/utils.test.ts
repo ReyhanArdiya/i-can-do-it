887438d86c91903fdc4740b91e9eea5bf9782fa8
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-10-06 20:46:11 +0700

    Reorganize `article` utils

--- src/models/article/utils.test.ts
+++ src/models/article/utils.test.ts
@@ -51,65 +51,67 @@ beforeEach(async () => {
 
 afterEach(async () => await cleanupFirebase(rules));
 
-test("saveArticle saves a new article", async () => {
-    const savedArticleRef = await saveArticle(db, mockArticle);
+describe("Firestore operations", () => {
+    test("saveArticle saves a new article", async () => {
+        const savedArticleRef = await saveArticle(db, mockArticle);
 
-    expect((await getDoc(savedArticleRef)).exists()).toBe(true);
-});
+        expect((await getDoc(savedArticleRef)).exists()).toBe(true);
+    });
 
-test("saveArticle updates an article", async () => {
-    const originalArticleRef = await saveArticle(db, mockArticle);
+    test("saveArticle updates an article", async () => {
+        const originalArticleRef = await saveArticle(db, mockArticle);
 
-    const newArticle: Article = {
-        ...mockArticle,
-        author: { picUrl: "meow", name: "Updated" },
-    };
+        const newArticle: Article = {
+            ...mockArticle,
+            author: { picUrl: "meow", name: "Updated" },
+        };
 
-    const newArticleData = (
-        await getDoc(await saveArticle(db, newArticle, originalArticleRef.id))
-    ).data() as Article;
+        const newArticleData = (
+            await getDoc(await saveArticle(db, newArticle, originalArticleRef.id))
+        ).data() as Article;
 
-    expect(newArticleData.author.name).toBe(newArticle.author.name);
-});
+        expect(newArticleData.author.name).toBe(newArticle.author.name);
+    });
 
-test("getArticle gets an article by its uid", async () => {
-    const savedArticleRef = await saveArticle(db, mockArticle);
+    test("getArticle gets an article by its uid", async () => {
+        const savedArticleRef = await saveArticle(db, mockArticle);
 
-    const fetchedArticle = (await getArticle(db, savedArticleRef.id)).data();
+        const fetchedArticle = (await getArticle(db, savedArticleRef.id)).data();
 
-    expect(fetchedArticle).toEqual(mockArticle);
-});
+        expect(fetchedArticle).toEqual(mockArticle);
+    });
 
-test("getArticles returns a list of article sorted by created", async () => {
-    const now = dayjs();
-    const articleOld: Article = {
-        ...mockArticle,
-        created: dayjs(now).subtract(1, "day").toDate(),
-    };
-    const articleNow = {
-        ...mockArticle,
-        created: now.toDate(),
-    };
-    const articleFuture = {
-        ...mockArticle,
-        created: dayjs(now).add(1, "day").toDate(),
-    };
+    test("getArticles returns a list of article sorted by created", async () => {
+        const now = dayjs();
+        const articleOld: Article = {
+            ...mockArticle,
+            created: dayjs(now).subtract(1, "day").toDate(),
+        };
+        const articleNow = {
+            ...mockArticle,
+            created: now.toDate(),
+        };
+        const articleFuture = {
+            ...mockArticle,
+            created: dayjs(now).add(1, "day").toDate(),
+        };
 
-    await saveArticle(db, articleOld);
-    await saveArticle(db, articleNow);
-    await saveArticle(db, articleFuture);
+        await saveArticle(db, articleOld);
+        await saveArticle(db, articleNow);
+        await saveArticle(db, articleFuture);
 
-    const newestToOldestArticles = (await getArticles(db)).docs;
+        const newestToOldestArticles = (await getArticles(db)).docs;
 
-    expect(newestToOldestArticles[0].data()).toEqual(articleFuture);
-    expect(newestToOldestArticles[1].data()).toEqual(articleNow);
-    expect(newestToOldestArticles[2].data()).toEqual(articleOld);
-});
+        expect(newestToOldestArticles[0].data()).toEqual(articleFuture);
+        expect(newestToOldestArticles[1].data()).toEqual(articleNow);
+        expect(newestToOldestArticles[2].data()).toEqual(articleOld);
+    });
 
-test("deleteArticle deletes an article by its uid", async () => {
-    const savedArticleRef = await saveArticle(db, mockArticle);
+    test("deleteArticle deletes an article by its uid", async () => {
+        const savedArticleRef = await saveArticle(db, mockArticle);
 
-    await deleteArticle(db, savedArticleRef.id);
+        await deleteArticle(db, savedArticleRef.id);
 
-    expect((await getDoc(savedArticleRef)).exists()).toBe(false);
+        expect((await getDoc(savedArticleRef)).exists()).toBe(false);
+    });
 });
