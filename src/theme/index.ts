ec497e416962ce222cc3b68c7c19fc55f5ea896d
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-10-15 19:57:59 +0700

    Setup `theme.shadows`

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,10 +1,12 @@
 import { extendTheme } from "@chakra-ui/react";
 import colors from "./colors";
+import shadows from "./shadows";
 import textStyles from "./text-styles";
 
 const theme = extendTheme({
     colors,
     textStyles,
+    shadows,
 });
 
 export default theme;
