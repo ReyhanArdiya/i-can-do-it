163cac547474ea6711da632d095a5c6a412a888d
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-10-23 00:03:08 +0700

    Create `useFetch` hook

--- src/hooks/use-fetch.tsx
+++ src/hooks/use-fetch.tsx
@@ -1,7 +1,8 @@
+import { useToast } from "@chakra-ui/react";
 import { useCallback, useEffect, useState } from "react";
 
-const useFetch = (dataFetcher: () => Promise<any>) => {
-    const [data, setData] = useState();
+const useFetch = <T extends () => Promise<any>>(dataFetcher: T) => {
+    const [data, setData] = useState<Awaited<ReturnType<T>>>();
     const [error, setError] = useState<Error | null>(null);
     const [isLoading, setIsLoading] = useState(true);
 
@@ -16,7 +17,21 @@ const useFetch = (dataFetcher: () => Promise<any>) => {
         }
 
         setIsLoading(false);
-    }, [dataFetcher]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const toast = useToast();
+
+    useEffect(() => {
+        error &&
+            toast({
+                status: "error",
+                title: error?.name,
+                description: error?.message,
+                duration: 5_000,
+                isClosable: true,
+            });
+    }, [error, toast]);
 
     return {
         fetchData,
