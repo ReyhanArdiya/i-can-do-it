96b58c2a94979c395bf4f09a42e0a9c5d0fd3c00
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-10-24 09:13:33 +0700

    Change `Navbar` style based on scroll

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,11 @@ import { Box, ChakraProvider } from "@chakra-ui/react";
 import { onAuthStateChanged, User } from "firebase/auth";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { MutableRefObject, useEffect, useRef, useState } from "react";
 import Footer from "../components/Footer";
 import HelpMenu from "../components/HelpMenu";
 import Navbar from "../components/Navbar";
+import useIsScrolled from "../hooks/use-is-scrolled";
 import theme from "../theme";
 import { auth } from "../utils/firebase/get-firebase-client";
 
@@ -26,10 +27,18 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         footerBg = "yellow.100";
     }
 
+    // Navbar change style based on scroll
+    const navbarRef = useRef<HTMLDivElement>(null);
+    const { isScrolled } = useIsScrolled(
+        navbarRef as MutableRefObject<HTMLDivElement>
+    );
+
     return (
         <ChakraProvider theme={theme}>
             {inAppRoutes && (
                 <Navbar
+                    scrolled={isScrolled}
+                    ref={navbarRef}
                     articleIconHref="/#articles"
                     gameIconHref="/#games"
                     logoHref="/"
