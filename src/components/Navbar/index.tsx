ce3685dc6101070eecc0dd3907e1c72b8a5212bd
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-10-24 08:51:27 +0700

    Add forwardRef to `Navbar`

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,14 @@
-import { HStack, Link, Text, theme, useMediaQuery } from "@chakra-ui/react";
+import {
+    HStack,
+    Link,
+    SystemStyleObject,
+    Text,
+    theme,
+    useMediaQuery,
+} from "@chakra-ui/react";
 import NextLink from "next/link";
 import { Books, GameController } from "phosphor-react";
+import { forwardRef } from "react";
 import useDevicesBreakpoints from "../../hooks/use-devices-breakpoints";
 
 import ICanDoItLogo from "../ICanDoItLogo";
@@ -15,71 +23,84 @@ export interface NavbarProps {
     scrolled?: boolean;
 }
 
-const Navbar = ({
-    userAvatarHref,
-    articleIconHref,
-    gameIconHref,
-    logoHref,
-    userAvatarPicSrc,
-    scrolled,
-}: NavbarProps) => {
-    const { isDesktop } = useDevicesBreakpoints();
+const Navbar = forwardRef<HTMLDivElement, NavbarProps>(
+    (
+        {
+            userAvatarHref,
+            articleIconHref,
+            gameIconHref,
+            logoHref,
+            userAvatarPicSrc,
+            scrolled,
+        },
+        ref
+    ) => {
+        const { isDesktop } = useDevicesBreakpoints();
+        const scrolledStyle = scrolled
+            ? {
+                  backdropFilter: "auto",
+                  backdropBlur: "sm",
+                  background: "rgba(255, 255, 255, 0.25)",
+              }
+            : {};
 
-    return (
-        <HStack
-            as="nav"
-            w="full"
-            px="3"
-            py="2"
-            spacing="0"
-            justifyContent="space-between"
-            pos="fixed"
-            top="0"
-            backdropFilter="auto"
-            backdropBlur="sm"
-            background="rgba(255, 255, 255, 0.25)"
-        >
-            <NextLink
-                passHref
-                href={logoHref}
+        return (
+            <HStack
+                as="nav"
+                w="full"
+                px="3"
+                py="2"
+                spacing="0"
+                justifyContent="space-between"
+                pos="fixed"
+                top="0"
+                ref={ref}
+                {...scrolledStyle}
             >
-                <Link>
-                    <ICanDoItLogo noText />
-                </Link>
-            </NextLink>
-
-            <HStack spacing="14">
                 <NextLink
-                    href={articleIconHref}
                     passHref
+                    href={logoHref}
                 >
                     <Link>
-                        <HStack spacing={1}>
-                            <Books size="32" />
-                            {isDesktop && <Text>Baca Artikel</Text>}
-                        </HStack>
+                        <ICanDoItLogo noText />
                     </Link>
                 </NextLink>
 
-                <NextLink
-                    href={gameIconHref}
-                    passHref
-                >
-                    <Link>
-                        <HStack spacing={1}>
-                            <GameController size="32" />
-                            {isDesktop && <Text>Main Game</Text>}
-                        </HStack>
-                    </Link>
-                </NextLink>
+                <HStack spacing="14">
+                    <NextLink
+                        href={articleIconHref}
+                        passHref
+                    >
+                        <Link>
+                            <HStack spacing={1}>
+                                <Books size="32" />
+                                {isDesktop && <Text>Baca Artikel</Text>}
+                            </HStack>
+                        </Link>
+                    </NextLink>
+
+                    <NextLink
+                        href={gameIconHref}
+                        passHref
+                    >
+                        <Link>
+                            <HStack spacing={1}>
+                                <GameController size="32" />
+                                {isDesktop && <Text>Main Game</Text>}
+                            </HStack>
+                        </Link>
+                    </NextLink>
+                </HStack>
+
+                <UserAvatar
+                    href={userAvatarHref}
+                    src={userAvatarPicSrc}
+                />
             </HStack>
+        );
+    }
+);
 
-            <UserAvatar
-                href={userAvatarHref}
-                src={userAvatarPicSrc}
-            />
-        </HStack>
-    );
-};
+Navbar.displayName = "Navbar";
 
 export default Navbar;
