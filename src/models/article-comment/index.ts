e47ce6139991f33cf64e1fa85648309733fffcb1
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-10-29 00:24:35 +0700

    Implement `ArticleComment` utils

--- src/models/article-comment/index.ts
+++ src/models/article-comment/index.ts
@@ -2,5 +2,9 @@ import { Author, HasAuthor } from "../types/author";
 import { WasCreated } from "../types/time";
 
 export default class ArticleComment implements HasAuthor, WasCreated {
-    constructor(public author: Author, public created: Date, public body: string) {}
+    constructor(
+        public author: Author & { uid: string },
+        public body: string,
+        public created: Date = new Date()
+    ) {}
 }
