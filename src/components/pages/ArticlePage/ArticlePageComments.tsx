fd784964eabfcc06322b9ee863ed2ee7592feac3
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-10-31 05:35:46 +0700

    Implement create article comment on `ArticlePage`

--- src/components/pages/ArticlePage/ArticlePageComments.tsx
+++ src/components/pages/ArticlePage/ArticlePageComments.tsx
@@ -1,12 +1,15 @@
-import { Button, Text, VStack } from "@chakra-ui/react";
+import { Button, Text, useDisclosure, VStack } from "@chakra-ui/react";
 import dayjs from "dayjs";
 import { useRouter } from "next/router";
 import { ChatTeardrop, Plus } from "phosphor-react";
-import { MouseEventHandler } from "react";
+import { MouseEventHandler, useState } from "react";
 import useIsAuth from "../../../hooks/use-is-auth";
 import ArticleComment from "../../../models/article-comment";
+import { saveArticleComment } from "../../../models/article-comment/utils";
+import { auth, db } from "../../../utils/firebase/get-firebase-client";
 import CommentCard from "../../Cards/CommentCard";
 import CircularIcon from "../../CircularIcon";
+import CommentModal from "../../Modals/CommentModal";
 
 export interface ArticlePageCommentsProps {
     articleId: string;
@@ -19,6 +22,31 @@ const ArticlePageComments = ({
     comments,
     onMoreCommentsButtonClick,
 }: ArticlePageCommentsProps) => {
+    // Modals
+    const { isOpen, onClose, onOpen } = useDisclosure();
+    const [commentBody, setCommentBody] = useState("");
+
+    const createComment = async () => {
+        try {
+            const { displayName, photoURL, uid } = auth.currentUser!;
+            await saveArticleComment(
+                db,
+                articleId,
+                new ArticleComment(
+                    {
+                        name: displayName!,
+                        picUrl: photoURL!,
+                        uid,
+                    },
+                    commentBody
+                )
+            );
+        } catch (err) {
+            console.error(err);
+        }
+        onClose();
+    };
+
     const commentCards = comments.map((comment, i) => (
         <CommentCard
             articleId={articleId}
@@ -46,6 +74,7 @@ const ArticlePageComments = ({
                     )}
                 />
             }
+            onClick={onOpen}
         >
             Buat Komentar
         </Button>
@@ -70,6 +99,14 @@ const ArticlePageComments = ({
             </Text>
 
             {commentButton}
+            <CommentModal
+                body={commentBody}
+                isOpen={isOpen}
+                onCancelClick={onClose}
+                onSaveClick={createComment}
+                onUpdateBody={e => setCommentBody(e.target.value)}
+            />
+
             <VStack spacing="inherit">{commentCards}</VStack>
 
             <Button
